fc4548c192b0129fa9453cfe08f22c7492e1bc06
Justin Lex-Hammarskjöld <33381901+JustinLex@users.noreply.github.com>
2023-12-03 02:31:04 +0100

    refactor: Move endpoints to endpoints.rs

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,19 +1,15 @@
-use actix_web::{get, web, App, HttpServer, Responder};
+use actix_web::{App, HttpServer};
 
-#[get("/")]
-async fn index() -> impl Responder {
-    "Hello, World!"
-}
-
-#[get("/{name}")]
-async fn hello(name: web::Path<String>) -> impl Responder {
-    format!("Hello {}!", &name)
-}
+mod endpoints;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(index).service(hello))
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+    HttpServer::new(|| {
+        App::new()
+            .service(endpoints::index)
+            .service(endpoints::hello)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
